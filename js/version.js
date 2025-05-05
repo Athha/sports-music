c775cda571dc516f6c5ccfbee707a8ab218c7aca
Athha <athha@users.noreply.github.com>
2025-05-05 23:53:10 +0900

    chore: バージョン1.3.0へのアップデート - IndexedDBストレージの正式リリース

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 console.log('version.js is executing');
 
-export const version = '1.2.14-indexeddb';
+export const version = '1.3.0';
 
 export function displayVersion() {
     console.log('Displaying version:', version);

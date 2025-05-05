9d7514342d965531c4b76290731751809c1f23e2
Athha <athha@users.noreply.github.com>
2025-05-05 22:34:21 +0900

    chore: バージョンを1.2.10-indexeddbに更新

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 console.log('version.js is executing');
 
-export const version = '1.2.9-indexeddb';
+export const version = '1.2.10-indexeddb';
 
 export function displayVersion() {
     console.log('Displaying version:', version);
